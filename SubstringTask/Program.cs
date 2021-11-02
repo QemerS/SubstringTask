56ad8444657f691c0bf59a69c5b5fa4c0fcd2f03
QemerS <tu201806169@code.edu.az>
2021-11-02 04:54:27 +0400

    Add project files.

---
+++ SubstringTask/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SubstringTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Metni daxil edin");
+            string text = Console.ReadLine();
+            if (text.Length > 50)
+            {
+                
+             Console.WriteLine(text.Substring(0, 50) + "...");
+            }
+            else { 
+            Console.WriteLine(text);
+            }
+        }
+    }
+}
